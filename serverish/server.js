8c25de66d286b747dfc0c90b60555437c88779ad
Michael/Windows <mkgrks@gmail.com>
2019-01-10 02:39:13 +0200

    first successful attempt

--- serverish/server.js
+++ serverish/server.js
@@ -1,14 +1,19 @@
 const fetch = require("node-fetch");
 const express = require("express");
 const cors = require("cors");
+const bodyParser = require("body-parser");
+const graphql = require("graphql-anywhere").default;
+const gql = require("graphql-tag");
 
 const app = express();
 var port = process.env.PORT || 2345;
 
 app.use(cors());
+app.use(bodyParser.json());
+
 
 app.get("/", (req, res) => {
-  console.log("empika mre");
+  console.log("empika mre"); // hahaha
   const msg = {
     hello: "Hello World"
   };
@@ -17,18 +22,24 @@ app.get("/", (req, res) => {
 
 // getKinos and parse request variables
 app.get("/getKinos", (req, res) => {
-  // console.log('request params', req.query);
-  const { date } = req.query;
-
-  // console.log('date:', date);
+  const { date, data } = req.query;
+  const fields = gql`${data}`;
+  const resolver = (fieldName, root) => root[fieldName];
+  
   // const apiUrl = `https://api.opap.gr/draws/v3.0/1100/draw-date/${date}/${date}?page=${page}`;
   const apiUrl = `https://applications.opap.gr/DrawsRestServices/kino/drawDate/${date}.json`;
-
-  // console.log(apiUrl);
+  
   fetch(apiUrl)
     .then(res => res.json())
     .then(data => {
-      res.send({ data });
+
+      const result = graphql(
+        resolver,
+        fields,
+        data
+      );
+
+      res.send({ result });
     })
     .catch(err => {
       // res.redirect('/error');
