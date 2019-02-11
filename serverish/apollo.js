2c144abc43349f08af007a45406e777906455630
Apostolakis Myron <mapostolakis@phaistosnetworks.gr>
2019-02-12 00:40:35 +0200

    class error

--- serverish/apollo.js
+++ serverish/apollo.js
@@ -5,27 +5,54 @@
 import express from 'express';
 import { ApolloServer, gql } from 'apollo-server-express';
 
+import StarWarsAPI from './datasources/StarWarsAPI';
 
+// http://api.openweathermap.org/data/2.5/forecast?id=6167865&APPID=9104e8c3dfac703a357aa39dcd429121
 const port = process.env.PORT || 2000;
 
 // Construct a schema, using GraphQL schema language
 const typeDefs = gql`
+  type Person {
+    name: String
+    films: [Film]
+  } 
+  type Film {
+    title: String
+  }
   type Query {
-    hello: String
+    people: [Person]
   }
 `;
  
 // Provide resolver functions for your schema fields
 const resolvers = {
   Query: {
-    hello: () => 'Hello world!',
+    people: (parent, args, context) => {
+      return context.dataSources.starWars.getPeople();
+    }
   },
+  Person: {
+    films: async (parent, args, context) => {
+      return Promise.all(
+        parent.films
+          .map((url) => parseSwapiURL(url).id)
+          .map((id) => context.dataSources.starWars.getFilm(id))
+      );
+    }
+  }
 };
- 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  dataSources: () => {
+    return {
+      starWars: new StarWarsAPI(),
+    };
+  },
+});
  
 const app = express();
-server.applyMiddleware({ app });
+server.applyMiddleware({ app, path: '/' });
  
 app.listen({ port }, () =>
   console.log(`🚀 Server ready at http://localhost:${port}${server.graphqlPath}`)
--- serverish/start_dummy.js
+++ serverish/start_dummy.js
@@ -6,4 +6,4 @@ require('babel-register')({
 })
 
 // Import the rest of our application.
-module.exports = require('./apollo.js')
+module.exports = require('./dummy.js')
