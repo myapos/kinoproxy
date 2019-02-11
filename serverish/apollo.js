0c2a91142476b748531546fb0272640094643df1
Apostolakis Myron <mapostolakis@phaistosnetworks.gr>
2019-02-12 01:01:04 +0200

    added parseSwapiUrl function

--- serverish/apollo.js
+++ serverish/apollo.js
@@ -1,12 +1,23 @@
 // https://www.apollographql.com/docs/apollo-server/essentials/server.html
 // https://www.reddit.com/r/graphql/comments/8kwldy/help_querying_data_from_a_3rd_party_api_with/
 // https://hackernoon.com/using-babel-7-with-node-7e401bc28b04
+// https://github.com/apollographql/apollo-server/issues/1217
 
 import express from 'express';
 import { ApolloServer, gql } from 'apollo-server-express';
 
 import StarWarsAPI from './datasources/StarWarsAPI';
 
+function parseSwapiURL(url) {
+  // "https://swapi.co/api/people/1/"
+  const parts = url.split('/');
+  return {
+    protocol: parts[0],
+    domain: parts[2],
+    model: parts[4],
+    id: parts[5],
+  };
+}
 // http://api.openweathermap.org/data/2.5/forecast?id=6167865&APPID=9104e8c3dfac703a357aa39dcd429121
 const port = process.env.PORT || 2000;
 
